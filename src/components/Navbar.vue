9717b0686f213c71a02c78d1b7307dee126917b2
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-09-20 22:45:57 -0300

    Navbar: Se agregan los elementos del navbar y se configura su incorporación en la vista Blog

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-white">
+    <div class="container">
+      <a href="https://www.rutero.cl">
+        <img class="retina-off" src="@/assets/logo-green.png" alt="Rutero">
+      </a>
+      <div class="collapse navbar-collapse">
+        <ul class="ml-auto navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,7 +1,9 @@
 <script setup>
+import Navbar from '../components/Navbar.vue'
 import Blog from '../components/Blog.vue'
 </script>
 
 <template>
+  <Navbar />
   <Blog />
 </template>
