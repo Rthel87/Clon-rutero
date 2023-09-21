231155e46398cdadc844df4408ef7b2f9b0b07b0
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-09-21 16:15:48 -0300

    Footer: Se agrega componente 'Footer' y se incorpora a la vista del 'Blog'

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-12 col-lg-12 col-xl-3">
+          <span class="footer-copyright"><img src="../assets/icon-footer.png" alt="icon">© RUTERO 2018</span>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
