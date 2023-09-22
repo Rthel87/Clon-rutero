08c3b386fdc4acd8e550fbd00f2500a795bc9bd7
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-09-21 21:54:50 -0300

    Blog: Se agrega nuevo componente 'Card' para mostrar el listado de blogs

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -3,5 +3,19 @@
 </script>
 
 <template>
-  <h1>Este es el Blog</h1>
+  <section class="section page-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-12 col-lg-11 col-xl-10">
+          <h1 class="page-title">Blog</h1>
+          <div class="page-content">
+            <div class="row pt-4">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="col-12 col-md-6 col-lg-6">
+    <div class="card camping-card">
+
+    </div>
+  </div>
+</template>
