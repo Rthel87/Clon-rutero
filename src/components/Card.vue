64154b4fe814ec4106f7918d36d65f61133b160e
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-06 01:49:40 -0300

    Blog: Se configura componente 'Card' con la información a mostrar de cada 'blog'

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,33 @@
 <script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['imgUrl', 'title', 'dataId'])
+
+const imgSource = 'src/assets/cards/' + props.imgUrl
+//const regexp = /[¿¡!\?\:áéíóú]/g
+const regexp = /[¿¡!\?\:]/g
+
+const titleLink = computed(() => {
+  let finalString = ''
+  if (props.title.length > 0) {
+    finalString = props.title.replaceAll(regexp, '')
+    finalString = finalString.normalize('NFD').replaceAll(/([aeio])\u0301|(u)[\u0301\u0308]/gi,"$1$2").normalize()
+    finalString = finalString.replaceAll(' ', '-').toLowerCase()
+  }
+  return "/blog/" + finalString
+})
 
 </script>
 
 <template>
   <div class="col-12 col-md-6 col-lg-6">
-    <div class="card camping-card">
-
+    <div class="card camping-card" :data-id="props.dataId">
+      <a class="camping-card-cover-container" :href="titleLink">
+        <img class="camping card-cover unveil" :src="imgSource" alt="">
+      </a>
+      <p class="camping-card-title">
+        <a :href="tittleLink">{{ props.title }}</a>
+      </p>
     </div>
   </div>
 </template>
