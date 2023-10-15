865726552da3dfff0c3c8a12acccdf2575eb89f0
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-10-14 23:50:10 -0300

    Blog: Se corrige comportamiento del componente 'Pagination'

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,6 +3,8 @@ import { computed } from 'vue'
 
 const props = defineProps(['visiting'])
 
+const blogDir = '/blog?page='
+
 const toShow = computed(() => {
   if (props.visiting >= 10) {
     return 14
@@ -19,21 +21,38 @@ const fromShow = computed(() => {
 
 const pages = computed(() => {
   let list = []
-  for (var i = fromShow; i <= toShow; i++) {
+  for (var i = fromShow.value; i <= toShow.value; i++) {
     list.push(i)
   }
   return list
 })
 
 const showPrev = computed(() => {
-  return fromShow > 1
+  return fromShow.value > 1
+})
+
+const showNext = computed(() => {
+  return toShow.value < 14
+})
+
+const prevPage = computed(() => {
+  return blogDir + (props.visiting - 1)
+})
+
+const nextPage = computed(() => {
+  return blogDir + (props.visiting + 1)
 })
 
 const linkAttributes = (page) => {
-  const dir = '/blog?page=' + page
+  const dir = blogDir + page
   if (page === props.visiting) {
     return {
       remote: 'false'
+    }
+  }
+  if (page === 1) {
+    return {
+      href: '/'
     }
   }
   return {
@@ -48,7 +67,7 @@ const linkAttributes = (page) => {
     <div class="col-12 pagination-container">
       <ul class="pagination justify-content-center">
         <li v-if="showPrev" class="page-item">
-          <a class="page-link prev" href="#" rel="prev">
+          <a class="page-link prev" :href="prevPage" rel="prev">
             <i class="fa fa-angle-left"></i>
           </a>
         </li>
@@ -56,15 +75,15 @@ const linkAttributes = (page) => {
           <a class="page-link" href="#">...</a>
         </li>
         <template v-for="page in pages">
-          <li class="page-item">
-            <a class="page-link" :class="{ page === props.visiting ? 'active' : '' }" v-bind="linkAttributes(page)">{{ page }}</a>
+          <li class="page-item" :class="page === props.visiting ? 'active' : ''">
+            <a class="page-link" v-bind="linkAttributes(page)">{{ page }}</a>
           </li>
         </template>
-        <li class="page-item disabled">
+        <li v-if="showNext" class="page-item disabled">
           <a class="page-link" href="#">...</a>
         </li>
-        <li class="page-item">
-          <a class="page-link next" href="#" rel="next">
+        <li v-if="showNext" class="page-item">
+          <a class="page-link next" :href="nextPage" rel="next">
             <i class="fa fa-angle-right"></i>
           </a>
         </li>
